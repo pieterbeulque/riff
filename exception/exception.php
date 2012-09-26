73fc2e38d86fdb932452116a61626cded86c64a3
Pieter Beulque <pieterbeulque@gmail.com>
2012-09-26 16:45:19 +0200

    Adds documentation

--- exception/exception.php
+++ exception/exception.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * Riff PHP Library
+ *
+ * This source file is part of Riff, a stand-alone PHP library.
+ *
+ * @package     Riff
+ * @subpackage  RiffException
+ *
+ * @author      Pieter Beulque <pieterbeulque@gmail.com>
+ * @since       0.1.1
+ *
+ */
+
+/**
+ * The standard exception used by Riff 
+ *
+ * @package     Riff
+ * @subpackage  RiffException
+ *
+ * @author      Pieter Beulque <pieterbeulque@gmail.com>
+ * @since       0.1.1
+ *
+ */
+
 class RiffException extends Exception
 {
 
--- riff/riff.php
+++ riff/riff.php
@@ -1,2 +1,25 @@
 <?php
 
+/**
+ * Riff PHP Library
+ *
+ * This source file is part of Riff, a stand-alone PHP library.
+ *
+ * @package     Riff
+ * @subpackage  RiffException
+ *
+ * @author      Pieter Beulque <pieterbeulque@gmail.com>
+ * @since       0.1.1
+ *
+ */
+
+/**
+ * The base class for Riff that handles autoloading and generic functions 
+ *
+ * @package     Riff
+ * @subpackage  RiffException
+ *
+ * @author      Pieter Beulque <pieterbeulque@gmail.com>
+ * @since       0.1.1
+ *
+ */
