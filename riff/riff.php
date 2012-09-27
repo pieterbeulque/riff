9a3920e2ab87109561e3a7608db1357e2286c097
Pieter Beulque <pieterbeulque@gmail.com>
2012-09-27 22:29:37 +0200

    Adds static dump function

--- riff/riff.php
+++ riff/riff.php
@@ -6,7 +6,7 @@
  * This source file is part of Riff, a stand-alone PHP library.
  *
  * @package     Riff
- * @subpackage  RiffException
+ * @subpackage  Riff
  *
  * @author      Pieter Beulque <pieterbeulque@gmail.com>
  * @since       0.1.1
@@ -17,9 +17,38 @@
  * The base class for Riff that handles autoloading and generic functions 
  *
  * @package     Riff
- * @subpackage  RiffException
+ * @subpackage  Riff
  *
  * @author      Pieter Beulque <pieterbeulque@gmail.com>
  * @since       0.1.1
  *
  */
+
+class Riff
+{
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * A nicer way to dump some variables in a readable way
+     * 
+     * @param mixed $var            Variable to dump
+     * @param bool[optional] $die   Quit execution after dumping?
+     */
+    public static function dump($var, $die = false)
+    {
+
+        ob_start();
+        echo '<pre style="font-family: ">';
+        var_dump($var);
+        echo '</pre>';
+        $dump = ob_get_clean();
+
+        echo $dump;
+        if ($die) die();
+    }
+
+}
