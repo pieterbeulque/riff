6a0cfcc9c15b5e324f77dd3befde908299f94a06
Pieter Beulque <pieterbeulque@gmail.com>
2012-09-29 11:16:57 +0200

    Adds autoloading

--- riff.php
+++ riff.php
@@ -24,11 +24,45 @@
  *
  */
 
+// Used charset
+if (!defined('RIFF_CHARSET')) define('RIFF_CHARSET', 'utf-8');
+
+// Debug mode or production mode
+if (!defined('RIFF_DEBUG')) define('RIFF_DEBUG', true);
+
+// Add the autoloader handle
+spl_autoload_register(array('Riff', 'autoload'));
+
 class Riff
 {
 
     public function __construct()
     {
+
+    }
+
+    public static function autoload($class)
+    {
+        // Riff has a convenient way of naming classes/files
+        // We can assume that a class called RiffException
+        // can be found in exception/exception.php
+
+        // We can erase the 'riff' part of the class
+        $class = ltrim(strtolower($class), 'riff');
+
+        // Ofcourse we cannot assume that everything will be named correctly
+        $exceptions = array();
+        $exceptions['riff'] = 'riff';
+
+        $path = dirname(realpath(__FILE__));
+
+        // If the class was not in the exceptions, check if it exists and include it
+        if (!in_array($class, $exceptions)) {
+            $file = $path . '/' . $class . '/' . $class . '.php';
+            if (file_exists($file)) require_once $file;
+        } else {
+            require_once $path . '/' . $exceptions[$class] . '.php';
+        }
 
     }
 
@@ -48,6 +82,7 @@ class Riff
         $dump = ob_get_clean();
 
         echo $dump;
+
         if ($die) die();
     }
 
