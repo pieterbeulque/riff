1b9a596e25192a7f06a2f4b6f49c7320f3f5a5ef
Pieter Beulque <pieterbeulque@gmail.com>
2012-10-09 15:13:51 +0200

    Introduces RiffJSON

---
+++ json/json.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Riff PHP Library
+ *
+ * This source file is part of Riff, a stand-alone PHP library.
+ *
+ * @package     Riff
+ * @subpackage  RiffJSON
+ *
+ * @author      Pieter Beulque <pieterbeulque@gmail.com>
+ * @since       0.1.1
+ *
+ */
+
+/**
+ * Allows for easy API calling via JSON in PHP
+ *
+ * @package     Riff
+ * @subpackage  RiffJSON
+ *
+ * @author      Pieter Beulque <pieterbeulque@gmail.com>
+ * @since       0.1.1
+ *
+ */
+
+class RiffJSON
+{
+    /**
+     * The decoded JSON object or array (specify in constructor)
+     * 
+     * @var object|array
+     */ 
+    private $decodedJSON;
+
+    /**
+     * The raw JSON output from the API
+     * 
+     * @var string
+     */ 
+    private $rawJSON;
+
+    /**
+     * The API url
+     * 
+     * @var string
+     */ 
+    private $url;
+
+    /**
+     * Call the URL and return the decoded information
+     * 
+     * @var string $url     The API URL
+     * @var bool $inArray   Array or object returned?
+     */ 
+    public function __construct($url, $inArray = false)
+    {
+        $this->url = (string) $url;
+        $this->rawJSON = file_get_contents($this->url);
+
+        if (!$this->rawJSON) {
+            throw new RiffException('Cannot connect to API');
+        }
+
+        $this->decodedJSON = json_decode($this->rawJSON, (bool) $inArray);
+
+        return $this->decodedJSON;
+    }
+}
