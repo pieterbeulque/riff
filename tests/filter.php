6cb4e474a9747972fb4b8698a76064bc1a3b28e0
Pieter Beulque <pieterbeulque@gmail.com>
2013-01-06 15:16:14 +0100

    Adds tests for filter

--- tests/filter.php
+++ tests/filter.php
@@ -8,8 +8,34 @@ class RiffFilterTest extends PHPUnit_Framework_TestCase
 {
     public function testCamelCaseToHyphen()
     {
-        $expected = 'this-is-camel-cased';
-        $result = RiffFilter::camelCaseToHyphen('ThisIsCamelCased');
+        $expected = 'this-is-hyphened';
+        $result = RiffFilter::camelCaseToHyphen('ThisIsHyphened');
         $this->assertEquals($expected, $result);
+    }
+
+    public function testHyphenToCamelCase()
+    {
+        $expected = 'thisIsHyphened';
+        $result = RiffFilter::hyphenToCamelCase('this-is-hyphened');
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDetectsWrongEmail()
+    {
+        $isEmail = RiffFilter::isEmail('not@nem.ail.');
+        $this->assertEquals(false, $isEmail);
+    }
+
+    public function testDetectsCorrectEmail()
+    {
+        $isEmail = RiffFilter::isEmail('test@test.com');
+        $this->assertEquals('test@test.com', $isEmail);
+    }
+
+    public function testUrlise()
+    {
+        $clean = 'societe-liberale';
+        $result = RiffFilter::urlise('Société Liberale!');
+        $this->assertEquals($clean, $result);
     }
 }
